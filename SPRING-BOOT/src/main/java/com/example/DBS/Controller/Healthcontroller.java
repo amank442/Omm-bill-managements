d89bb073b6dc3a82835ab3a2d675039a2c6f131a
Aman <amanhzb33@gmail.com>
2025-06-03 13:31:23 +0530

    Jenkins file

---
+++ SPRING-BOOT/src/main/java/com/example/DBS/Controller/Healthcontroller.java
@@ -0,0 +1,14 @@
+package com.example.DBS.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Healthcontroller {
+
+
+@GetMapping("/health")
+public ResponseEntity<String> healthcheck(){
+    return ResponseEntity.OK("Spring Boot svc runnig.")
+}
+}
